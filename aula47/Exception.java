117085e336e40229e75c07a424802ba05b8fabd3
Daniel Alves <danielalves.ccomp@gmail.com>
2021-04-21 23:02:09 -0300

    Currency Formatter

--- aula47/Exception.java
+++ aula47/Exception.java
@@ -3,22 +3,22 @@ package aula47;
 public class Exception {
 
 	public static void main(String[] args) {
-		
+
 		try {
 			int[] vetor = new int[4];
-			
+
 			System.out.println("Before the exception");
-						
+
 			vetor[4] = 5;
-			
+
 			System.out.println("Not print");
-		
+
 		} catch(ArithmeticException | ArrayIndexOutOfBoundsException exception) {
-			
+
 			System.out.println("Exception when accessing an array index that doesnt exist");
-		
+
 		}
-		
+
 		System.out.println("After the exception");		
 
 	}
